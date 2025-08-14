2fe96719fbeee1c6e0ab736547a10d0c93e67748
guiopauer <guiopauer@hotmail.com>
2025-08-13 21:26:44 -0300

    Add auth (login/register) interface

--- voce_fit/src/app/app.routes.ts
+++ voce_fit/src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { WorkoutDetailComponent } from './pages/workout-detail/workout-detail.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: IndexComponent },
-  { path: 'workouts/:id', component: WorkoutDetailComponent },
-  { path: '**', component: NotFoundComponent }
+  { 
+    path: '', 
+    component: IndexComponent,
+    canActivate: [AuthGuard]
+  },
+  { 
+    path: 'workout/:id', 
+    component: WorkoutDetailComponent,
+    canActivate: [AuthGuard]
+  },
+  { path: 'auth', component: AuthComponent },
+  { path: '**', redirectTo: '' }
 ];
